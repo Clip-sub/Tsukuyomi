8032a63fd0014f3dbd0f71b5892d7f72992378aa
Quan Pham <doraemonfanclub@gmail.com>
2018-08-24 02:43:45 +0700

    Minor fix

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,15 @@ import string
 
 import flask
 from flask_assets import Bundle
+
+def create_app(config):
+  app = flask.Flask(__name__)
+  app.config.from_object(config)
+
+  app.config['SESSION_REFRESH_EACH_REQUEST'] = False
+
+  # Debugging:
+  if app.config['DEBUG']:
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+  return app
