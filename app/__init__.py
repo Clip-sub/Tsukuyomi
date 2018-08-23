96417aaf8e15ee50cc916a6ce9ea9bea70d5859c
Quan Pham <doraemonfanclub@gmail.com>
2018-08-24 02:34:05 +0700

    Added app creation factory

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+import logging
+import os
+import string
+
+import flask
+from flask_assets import Bundle
---
+++ dev.py
@@ -0,0 +1,11 @@
+import sys
+
+TEST_PATHS = ['tests']
+
+def print_cmd(cmd, args):
+  print('Running: {0}\n'.format(
+    ' '.join([('\'' + a + '\'' if ' ' in a else a) for a in [cmd] + args])))
+  sys.stdout.flush()
+
+if __name__ == '__main__':
+  assert sys.version_info >= (3, 6), "Python 3.6 or above is required"
