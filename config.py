c588c2af502b1a9212fa7bf6ce27a8bea5b5b145
Quan Pham <doraemonfanclub@gmail.com>
2018-08-24 02:47:46 +0700

    Added config.py

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
 import os
 import re
+
+DEBUG = True
+
+# A read-only maintenance mode, in which the database is not modified
+MAINTENANCE_MODE = False
+# A maintenance message (used in layout.html template)
+MAINTENANCE_MODE_MESSAGE = 'Site is currently in read-only maintenance mode.'
+# Allow logging in during maintenance (without updating last login date)
+MAINTENANCE_MODE_LOGINS = True
