a5cfc1df1a1da8b3726b13186eeca1578dc5e567
Quan Pham <doraemonfanclub@gmail.com>
2018-08-31 03:23:43 +0700

    Added config example

--- config.example.py
+++ config.example.py
@@ -11,7 +11,6 @@ MAINTENANCE_MODE_MESSAGE = 'Site is currently in read-only maintenance mode.'
 MAINTENANCE_MODE_LOGINS = True
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQL_DATABASE_URI = ''
 DATABASE_CONNECT_OPTIONS = {}
 
 CSRF_ENABLED = True
@@ -20,4 +19,11 @@ CSRF_SESSION_KEY = 'd731ad92-82ef-48f1-8844-ae935b2761d8'
 # Use MySQL or Sqlite3 (mostly deprecated)
 USE_MYSQL = True
 
+# Enable password recovery (by reset link to given email address)
+# Depends on email support!
 ALLOW_PASSWORD_RESET = True
+
+if USE_MYSQL:
+  SQLALCHEMY_DATABASE_URI = ('mysql://test:test123@localhost/tsukuyomi?charset=utf8mb4')
+else:
+  SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(BASE_DIR, 'test.db') + '?check_same_thread=False')
