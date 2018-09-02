94b856713af97542337c03af36b586b66a8fbd6e
Quan Pham <doraemonfanclub@gmail.com>
2018-09-03 04:11:44 +0700

    Added helper

--- dev.py
+++ dev.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def hello_world():
   return 'Hello World!!!'
 
+def print_help():
+    print('Tsukuyomi Development Helper')
+    print('=======================\n')
+    print('Usage: {0} command [different arguments]'.format(sys.argv[0]))
+    print('Command can be one of the following:\n')
+    print('  lint | check       : do a lint check (flake8 + flake8-isort)')
+    print('  fix  | autolint    : try and auto-fix lint (autopep8)')
+    print('  isort              : fix import sorting (isort)')
+    print('  test | pytest      : run tests (pytest)')
+    print('  help | -h | --help : show this help and exit')
+    print('')
+    print('You may pass different arguments to the script that is being run.')
+    print('For example: {0} test tests/ --verbose'.format(sys.argv[0]))
+    print('')
+    return 1
+
 def print_cmd(cmd, args):
   print('Running: {0}\n'.format(
     ' '.join([('\'' + a + '\'' if ' ' in a else a) for a in [cmd] + args])))
