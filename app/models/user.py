dc30a023e67e9c5d81d22695414de05d86e4e97d
Quan Pham <doraemonfanclub@gmail.com>
2018-09-04 01:46:18 +0700

    Eject pylintrc

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,8 @@
 from app.extensions import db
-from flask import Flask
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   email = db.Column(db.String(120), unique = True, nullable = False)
+
+  def __repr__(self):
+    return '<User %r>' % self.email
