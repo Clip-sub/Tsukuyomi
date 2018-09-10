d79b6e82454b11e5906c5b6519e0e95e3b4b1597
Quan Pham <doraemonfanclub@gmail.com>
2018-09-11 02:28:27 +0700

    Switch to Pycharm, use pep8 from now on but still keep pylintrc file

--- app/models/manga.py
+++ app/models/manga.py
@@ -1,1 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
+from dev import db
+
+
+class Manga(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
