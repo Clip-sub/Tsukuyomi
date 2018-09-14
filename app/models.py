0b6d07047e1eda6ce61126c6942517c6abb620d1
Quan Pham <doraemonfanclub@gmail.com>
2018-09-14 10:46:45 +0700

    Define data types for specific DB types

--- app/models.py
+++ app/models.py
@@ -1,4 +1,22 @@
 from app.extensions import db
+import config
+
+if config.USE_MYSQL:
+    from sqlalchemy.dialects import mysql
+
+    BinaryType = mysql.BINARY
+    TextType = mysql.TEXT
+    MediumBlobType = mysql.MEDIUMBLOB
+    COL_UTF8_GENERAL_CI = 'utf8_general_ci'
+    COL_UTF8MB4_BIN = 'utf8mb4_bin'
+    COL_ASCII_GENERAL_CI = 'ascii_general_ci'
+else:
+    BinaryType = db.Binary
+    TextType = db.String
+    MediumBlobType = db.BLOB
+    COL_UTF8_GENERAL_CI = 'NOCASE'
+    COL_UTF8MB4_BIN = None
+    COL_ASCII_GENERAL_CI = 'NOCASE'
 
 
 class User(db.Model):
