c0fabd433934549db5d10295e24534ed20fdd531
Quan Pham <doraemonfanclub@gmail.com>
2018-11-10 20:31:32 +0700

    Added LOGGER

--- app/extensions.py
+++ app/extensions.py
@@ -1,15 +1,26 @@
 import os.path
+from logging import getLogger
 
 from flask.config import Config
+from flask.ext.celery import Celery
+from flask.ext.redis import Redis
 from flask_assets import Environment
 from flask_caching import Cache
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.event import listens_for
 
+LOG = getLogger(__name__)
 assets = Environment()
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
 cache = Cache(config={'CACHE-TYPE': 'simple'})
+
+
+@listens_for(Pool, 'connect', named=True)
+def _on_connect(dbapi_connection, **_):
+    LOG.debug('Setting SQL Mode to TRADITIONAL')
+    dbapi_connection.cursor().execute("SET SESSION sql_mode='TRADITIONAL'")
 
 
 def _get_config():
@@ -19,4 +30,7 @@ def _get_config():
     return cfg
 
 
+celery = Celery()
+db = SQLAlchemy()
+redis = Redis()
 config = _get_config()
